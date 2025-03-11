baa61aeb76b68f732cebb71f66977b8d3f31fe58
Omar Faruk <omarfaruk.dev@gmail.com>
2025-03-11 16:49:07 +0600

    chapter 3 - data type practice completed

---
+++ c - chapter - 3 (Data Type)/5 - parse/practice-3.js
@@ -0,0 +1,7 @@
+/* 
+৩. তুই 'premik Boy' নামের একটা স্ট্রিংকে সংখ্যা বানানোর চেষ্টা কর। দেখ কী আউটপুট দেয়।
+ */
+let boysType = 'premik Boy';
+let convertedToNumber = parseInt(boysType);
+
+console.log(convertedToNumber); // NaN
