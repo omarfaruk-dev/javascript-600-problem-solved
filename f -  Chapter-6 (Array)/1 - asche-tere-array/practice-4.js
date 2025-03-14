3eb5b43e915a8f7bc2d425334ea00c57b1515c01
Omar Faruk <omarfaruk.dev@gmail.com>
2025-03-14 15:01:32 +0600

    chapter 6 - array practice completed

---
+++ f -  Chapter-6 (Array)/1 - asche-tere-array/practice-4.js
@@ -0,0 +1,5 @@
+/* 
+৪. তোর 5 টা প্রিয় সিনেমার নাম একটা অ্যারেতে রাখ।
+ */
+const movies = ['The Message','History of Masjidul Aqsa', 'Omar LifeStyle', 'Islamic Culture', 'Mohammd (SM) the final legacy'];
+console.log(movies);
